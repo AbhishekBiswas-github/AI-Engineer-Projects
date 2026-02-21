d6c14d85b55cd2f06781d7c7108c3fa27f10eea0
Abhishek Biswas <67058151+AbhishekBiswas-github@users.noreply.github.com>
2026-02-21 12:25:01 +0530

    Add files via upload

--- Streamlit_Projects/Loan Approval Tool/app.py
+++ Streamlit_Projects/Loan Approval Tool/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
 
 import charts as ct
 import model as ml
@@ -39,18 +40,31 @@ st.title("Loan Approval Tool")
 if not st.session_state.file_uploaded:
     st.subheader('Upload your file', divider='rainbow')
 
-    upload_file = st.file_uploader("", type='csv')
-    if upload_file is not None:
-        st.toast("😄 File uploaded successfully")
-        st.session_state.dataset = pd.read_csv(upload_file)
+    if st.button("Load Dataset", type='primary'):
+        my_bar = st.progress(0, "Reading the Dataset")
+        
+        for percentage_complete in range(100):
+            time.sleep(0.01)
+            my_bar.progress(percentage_complete+1, "Reading the Dataset")   
 
+        time.sleep(1)
+        my_bar.empty()
+
+        url = "https://raw.githubusercontent.com/AbhishekBiswas-github/AI-Engineer-Projects/refs/heads/main/Machine-Learning/Loan-Approval-Prediction/loan_approval_dataset.csv"
+
+        df = pd.read_csv(url)
+        st.toast("😄 File uploaded successfully")
+        st.session_state.dataset = df
+
+        st.info("Dataset Loaded Successfully")
         if st.button(label="Show Insights", on_click=file_uploaded, type='primary'):
             st.write("File uploaded, show Dataset....")
-
+                # st.rerun()
 
 # Show insights page
-if st.session_state.file_uploaded and not st.session_state.insights_shown and not st.session_state.model_created:
+elif st.session_state.file_uploaded and not st.session_state.model_created:
     ct.show_insights()
+
     if st.button(label="Create Model", on_click=model_created, type='primary'):
         st.write("Insights Shown....")
         
@@ -59,9 +73,10 @@ if st.session_state.file_uploaded and not st.session_state.insights_shown and no
 if st.session_state.model_created and not st.session_state.predicted:
     st.header("Model Creation and Prediction", divider="violet")
     ml.create_model()
+
     if st.button(label="Start Prediction", on_click=start_prediction, type='primary'):
         st.write("Start Prediction....")
 
-if st.session_state.predicted:
+elif st.session_state.predicted:
     st.header('Fill the Form to check the status of your loan', divider='rainbow')
     pred.start_prediction()
